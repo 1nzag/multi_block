efece8a1966c11739496a2a15e06562c0dccabb3
1nzag <wkdwns420@gmail.com>
2017-11-07 16:42:55 -0800

    Make header file

--- find_db.c
+++ find_db.c
@@ -42,10 +42,6 @@ COPYRIGHT (C) 1986 Gary S. Brown.  You may use this program, or
  * CRC32 code derived from work by Gary S. Brown.
  */
 
-#include <sys/param.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
 
 static uint32_t crc32_tab[] = {
 	0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f,
---
+++ find_db.h
@@ -0,0 +1,13 @@
+#include <sys/param.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#ifndef __find_url_h__
+#define __find_uel_h__
+
+int find_url(char filename, char* string);
+
+#endif
+
